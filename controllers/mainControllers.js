de4be1f9aeb240d79e9ba8dbcb7e50393cd21035
Hernan Gomez <hernangomez@MacBook-Pro-de-Hernan.local>
2020-05-14 07:05:19 -0300

    Estructura express + EJS + Controllers + index.html

---
+++ controllers/mainControllers.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+// ************ Function to Read an HTML File ************
+function readHTML (fileName) {
+	let htmlFile = fs.readFileSync(path.join(__dirname, `/../views/${fileName}.html`), 'utf-8');
+	return htmlFile;
+}
+
+const controller = {
+	root: (req, res) => {
+		let html = readHTML('index');
+		res.send(html);
+	}
+};
+
+module.exports = controller
