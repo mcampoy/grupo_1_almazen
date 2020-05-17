e5e3afcdbf76a306e534ae5091e13040c16c847d
Hernan Gomez <hernangomez@MacBook-Pro-de-Hernan.local>
2020-05-17 17:10:14 -0300

    modificaciones index y product

--- site/routes/products.js
+++ site/routes/products.js
@@ -5,4 +5,5 @@ const productsControllers = require('../controllers/productsControllers');
 router.get('/details', productsControllers.details);
 router.get('/edit', productsControllers.edit);
 
+
 module.exports = router;
