f05073bd856f51cd484b9eefec72b8de19d53af1
Matías Campoy <matiascampoy@gmail.com>
2020-06-16 17:10:37 -0300

    cambios en la páginad e receta

--- site/src/controllers/recetasController.js
+++ site/src/controllers/recetasController.js
@@ -2,6 +2,17 @@ const fs = require('fs');
 const path = require('path');
 
 const recetas = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/recetasDataBase.json'), 'utf-8'));
+let productsPath = path.resolve(__dirname, '../data/productsDataBase.json');
+const products = getProducts();
+function getProducts() {
+  let productsJson = fs.readFileSync(productsPath, 'utf-8');
+
+  if (productsJson != ' ') {
+      return JSON.parse(productsJson)
+  } else {
+      return []
+  }
+};
 
 const controller = {
 
@@ -13,15 +24,17 @@ const controller = {
 
     receta: (req, res) => {
 
+      
+      
       const receta = recetas.find((receta)=>{
-            return receta.id == req.params.id;
-          })
-
+        return receta.id == req.params.id;
+      })
+           
           if(receta == null){
 
             return res.redirect('/');
           }
-        res.render('receta', {receta: receta});
+        res.render('receta', {receta, products});
     },
 
     category: (req, res) => {
