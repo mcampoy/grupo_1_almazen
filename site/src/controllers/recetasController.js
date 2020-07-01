1e7805ffd63e71f99a8ca9e5b7e2930184df8ace
Matías Campoy <matiascampoy@gmail.com>
2020-07-01 07:19:28 -0300

    Recetas casi listas. Falta enviarle los productos a la vista de receta

--- site/src/controllers/recetasController.js
+++ site/src/controllers/recetasController.js
@@ -1,21 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 let db = require('../database/models');
-let sequelize = db.sequelize;
-
-const recetas = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/recetasDataBase.json'), 'utf-8'));
-let productsPath = path.resolve(__dirname, '../data/productsDataBase.json');
-const products = getProducts();
-
-function getProducts() {
-    let productsJson = fs.readFileSync(productsPath, 'utf-8');
-
-    if (productsJson != ' ') {
-        return JSON.parse(productsJson)
-    } else {
-        return []
-    }
-};
 
 const controller = {
 
@@ -46,9 +31,9 @@ const controller = {
                     return res.redirect('/');
                 }
                 if (req.session.usuarioLogueado == undefined) {
-                    return res.render('receta', { receta, products, usuarioLogueado: undefined });
+                    return res.render('receta', { receta, usuarioLogueado: undefined });
                 } else {
-                    return res.render('receta', { receta, products, usuarioLogueado: req.session.usuarioLogueado });
+                    return res.render('receta', { receta, usuarioLogueado: req.session.usuarioLogueado });
                 }
             })
             .catch((err) => console.error(err));
@@ -57,41 +42,25 @@ const controller = {
 
     category: (req, res) => {
 
-        var sinGluten = db.Recipe.findAll({
+        db.Recipe.findAll({
             where: {
-                diet: "sin gluten"
+                diet: req.params.dieta
             }
         })
-
-        console.log(sinGluten)
-
-        const receta = recetas.find((receta) => {
-            return receta.dieta == req.params.dieta;
-        })
-        let dietas = [];
-        let otras = [];
-
-        for (let receta of recetas) {
-            if (receta.dieta == req.params.dieta) {
-                dietas.push(receta);
+        .then((recetas)=>{
+            if (req.session.usuarioLogueado == undefined) {
+                return res.render('recetasPorDietas', { recetas: recetas, usuarioLogueado: undefined });
             } else {
-                otras.push(receta);
+                return res.render('recetasPorDietas', { recetas: recetas, usuarioLogueado: req.session.usuarioLogueado });
             }
-        }
+        })
 
-        //res.render('recetasPorDietas', {dietas, receta});
-        if (req.session.usuarioLogueado == undefined) {
-            return res.render('recetasPorDietas', { dietas, receta, usuarioLogueado: undefined });
-        } else {
-            return res.render('recetasPorDietas', { dietas, receta, usuarioLogueado: req.session.usuarioLogueado });
-        }
     },
 
     listaCompras: (req, res) => {
         let prodSelecc = {...req.body };
         console.log(prodSelecc);
         res.send('hola', { prodSelecc })
-
     }
 };
 
