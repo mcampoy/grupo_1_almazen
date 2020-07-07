6d705836fd918b1f8abd0ec85c80a58b0c0c7b5a
Matías Campoy <matiascampoy@gmail.com>
2020-07-07 16:44:03 -0300

    Se implementó el buscador

--- site/src/routes/products.js
+++ site/src/routes/products.js
@@ -20,6 +20,7 @@ router.delete('/admin', usersMiddlewares.adminValidation, productsMiddlewares.de
 router.put('/admin', usersMiddlewares.adminValidation, productsMiddlewares.editProductValidation, middUploadFile.uploadFile, productsControllers.adminUpdate);
 router.post('/admin', usersMiddlewares.adminValidation, productsMiddlewares.newProductValidation, middUploadFile.uploadFile, productsControllers.adminCreate);
 
+// Buscador
 router.post('/find', productsControllers.find)
 
 module.exports = router;
