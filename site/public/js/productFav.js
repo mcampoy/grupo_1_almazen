e39f2941a9e4717d7bf6283433faa65623da7f14
Matías Campoy <matiascampoy@gmail.com>
2020-07-21 16:59:03 -0300

    Mejoras en el buscador dinámico. Ahora las búsquedas deben tener dor caracteres como mínimo

--- site/public/js/productFav.js
+++ site/public/js/productFav.js
@@ -1,39 +1,39 @@
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
 
     let savedFavs = JSON.parse(localStorage.getItem("prodFavs"));
-if (!savedFavs) {
-    savedFavs = [];
-}
+    if (!savedFavs) {
+        savedFavs = [];
+        console.log(savedFavs)
+    }
 
 
-     // CAMBIAR CORAZÓN EN FAVORITO
-     
-     let favs = document.querySelectorAll('i.prodFav')
-     for (let fav of favs) {
-         
-         fav.addEventListener('click', function () {
-             
-           
-             if (fav.classList.contains('far')) {
-                 fav.classList.remove('far')
-                 fav.classList.add('fas')
+    // CAMBIAR CORAZÓN EN FAVORITO
 
-                 let prods = document.querySelectorAll('div.prodFavId')
-                
-                 for( let i = 0; i < prods.length; i++ ){
-                    let guardar =  prods[i].id
-                      localStorage.setItem("favsProd", prods[i].id)
-                      console.log(guardar)
-                    }
+    let favs = document.querySelectorAll('i.prodFav')
+    for (let fav of favs) {
 
-             } else {
-                 fav.classList.remove('fas')
-                 fav.classList.add('far')
-             }
+        fav.addEventListener('click', function () {
+            savedFavs.push(fav)
+
+            if (fav.classList.contains('far')) {
+                fav.classList.remove('far')
+                fav.classList.add('fas')
 
 
-         })
-     }
+                // for (let i = 0; i < prods.length; i++) {
+                //     let guardar = prods[i].id
+                //     localStorage.setItem("favsProd", prods[i].id)
+                //     console.log(guardar)
+                // }
+
+            } else {
+                fav.classList.remove('fas')
+                fav.classList.add('far')
+            }
+
+
+        })
+    }
 
 
 })
--- site/public/js/search.js
+++ site/public/js/search.js
@@ -20,40 +20,43 @@ window.addEventListener('load', function () {
             })
             .then((info) => {
 
-                console.log(info)
-
                 let div = document.getElementById('divSerch')
 
-                if(!filter){
-                    div.innerHTML = ""
+                if (filter === '') {
+
+                    div.innerHTML = ''
+
+                }else if(filter.length === 1) {
+
+                    div.innerHTML = '<ul id="ulSearch" >' + '<li class="minSearch" style="padding-top: 1%">' + 'Debe ingresar 2 caracteres como mínimo' + '</li>' + '</ul>'
 
                 } else {
                     let searchList = []
 
                     for (let i = 0; i < info.data.products.length; i++) {
 
-                    let discount = info.data.products[i].discount > 1 ? info.data.products[i].discount + '% OFF': ''
+                        let discount = info.data.products[i].discount > 1 ? info.data.products[i].discount + '% OFF' : ''
 
-                    let productadd = '<li>' + '<a href="/product/details/' + info.data.products[i].id + '">' + '<img class="searchImg" src="/images/imgProductos/' + info.data.products[i].image + '">' + '<div>' + '<span class="searchCategory">' + 'Producto' + '</span>' + '<span class="searchName">' + info.data.products[i].name + '</span>' + '<span class="searchPrice">' + '$' + info.data.products[i].price + '</span>' + '   ' + '<span class="searchDiscount">' + discount  + '</span>' + '</div>' + '</a>' + '</li>'
+                        let productadd = '<li>' + '<a href="/product/details/' + info.data.products[i].id + '">' + '<img class="searchImg" src="/images/imgProductos/' + info.data.products[i].image + '">' + '<div>' + '<span class="searchCategory">' + 'Producto' + '</span>' + '<span class="searchName">' + info.data.products[i].name  + '  ' + '<span class="searchQuantity">' + info.data.products[i].quantity + ' ' + info.data.products[i].unit + '</span>' + '</span>' + '<span class="searchPrice">' + '$' + info.data.products[i].price + '</span>' + '   ' + '<span class="searchDiscount">' + discount + '</span>' + '</div>' + '</a>' + '</li>'
 
-                    searchList.push(productadd)
+                        searchList.push(productadd)
                     }
 
                     for (let i = 0; i < info.data.recetas.length; i++) {
-                    let recipadd = '<li>' + '<a href="/recetas/' + info.data.recetas[i].id + '">' + '<img class="searchImg" src="/images/recetas/' + info.data.recetas[i].image + '">' + '<div>' + '<span class="searchCategory">' + 'Receta' + '</span>' + '<span class="searchName">' + info.data.recetas[i].name + '</span>' + '</div>' + '</a>' + '</li>'
+                        let recipadd = '<li>' + '<a href="/recetas/' + info.data.recetas[i].id + '">' + '<img class="searchImg" src="/images/recetas/' + info.data.recetas[i].image + '">' + '<div>' + '<span class="searchCategory">' + 'Receta' + '</span>' + '<span class="searchName">' + info.data.recetas[i].name + '</span>' + '</div>' + '</a>' + '</li>'
 
-                    searchList.push(recipadd)
+                        searchList.push(recipadd)
+                    }
+
+                    if (searchList == 0) {
+                        div.innerHTML = '<ul id="ulSearch">' + '<li class="noFoundSearch">' + 'No hay resultados que coincidan con la búsqueda' + '</li>' + '</ul>'
+                    } else {
+
+                        div.innerHTML = '<ul id="ulSearch">' + searchList + '</ul>'
+                    }
+
+
                 }
-
-                if (searchList == 0) {
-                    div.innerHTML = '<ul id="ulSearch">' +  '<li class="noFoundSearch">' + 'No hay resultados que coincidan con la búsqueda' + '</li>'  + '</ul>'   
-                } else {
-                    
-                    div.innerHTML = '<ul id="ulSearch">' +  searchList  + '</ul>'
-                }
-
-
-            }
             })
             .catch(err => console.error(err))
     })
