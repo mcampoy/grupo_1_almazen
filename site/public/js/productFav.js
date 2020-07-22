2f5ada683a887a64726a022aae0ce6c51d69f774
rafavidela <rvidela@gmail.com>
2020-07-22 03:15:16 -0300

    Se modificó productFav.js para guardar en localStorage el id de productos favoritos

--- site/public/js/productFav.js
+++ site/public/js/productFav.js
@@ -1,9 +1,8 @@
-window.addEventListener("load", function () {
+window.addEventListener("load", function() {
 
     let savedFavs = JSON.parse(localStorage.getItem("prodFavs"));
     if (!savedFavs) {
         savedFavs = [];
-        console.log(savedFavs)
     }
 
 
@@ -12,28 +11,51 @@ window.addEventListener("load", function () {
     let favs = document.querySelectorAll('i.prodFav')
     for (let fav of favs) {
 
-        fav.addEventListener('click', function () {
-            savedFavs.push(fav)
+        fav.addEventListener('click', function() {
+            //savedFavs.push(fav)
+
+            let prodId = this.id;
+            console.log(prodId);
 
             if (fav.classList.contains('far')) {
                 fav.classList.remove('far')
                 fav.classList.add('fas')
 
+                //Antes de agregarlo al array de favoritos se fija si ya existe
+                let exists = savedFavs.filter(item => {
+                    if (item.id == prodId)
+                        return true;
+                });
 
-                // for (let i = 0; i < prods.length; i++) {
-                //     let guardar = prods[i].id
-                //     localStorage.setItem("favsProd", prods[i].id)
-                //     console.log(guardar)
-                // }
+                //Si no existe lo agrega
+                if (!exists[0]) {
+                    savedFavs.push({ "id": prodId });
+                    let favs = JSON.stringify(savedFavs);
+                    localStorage.setItem("prodFavs", favs);
+                }
+
+                //  let prods = document.querySelectorAll('div.prodFavId')
+
+                //  for( let i = 0; i < prods.length; i++ ){
+                //     let guardar =  prods[i].id
+                //       localStorage.setItem("favsProd", prods[i].id)
+                //       console.log(guardar)
+                //     }
 
             } else {
                 fav.classList.remove('fas')
                 fav.classList.add('far')
+
+                //Lo quita de la lista de favoritos
+                savedFavs = savedFavs.filter(item => {
+                    if (item.id !== prodId)
+                        return true;
+                });
+
+                //Transforma en Json y lo guarda en localStorage
+                let favs = JSON.stringify(savedFavs);
+                localStorage.setItem("prodFavs", favs);
             }
-
-
         })
     }
-
-
 })
